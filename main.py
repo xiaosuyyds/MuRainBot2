515c4d2e30a300dc5bbfb7bd7b7d664b99eddc96
xiaosuyyds <Xiaosu-1009@qq.com>
2024-02-17 20:00:48 +0800

    新建插件扫描

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ from Lib import MuRainLib
 from flask import Flask
 import yaml
 import os
+import importlib
 from werkzeug.serving import make_server
 
 logger = MuRainLib.log_init()
 VERSION = "2.0.0-dev"  # 版本
 VERSION_WEEK = "24Y07W"  # 版本周
 
+plugins = {}  # 插件
 app = Flask(__name__)
 
 
@@ -40,10 +42,36 @@ def import_profile(yml_path):
         return None
 
 
+def import_plugins(plugins_path):
+    global plugins
+    # 获取插件目录下的所有文件
+    things_in_plugin_dir = os.listdir(plugins_path)
+
+    # 筛选出后缀为.py的文件
+    def pick_module(name, plugin_suffix=".py"):
+        if name.endswith(plugin_suffix):
+            return name.split(".")[0]
+        else:
+            return ""
+
+    things_in_plugin_dir = map(pick_module, things_in_plugin_dir)
+    things_in_plugin_dir = [_ for _ in things_in_plugin_dir if _ != ""]
+
+    plugins = {}
+
+    for i in things_in_plugin_dir:
+        try:
+            plugins[i] = importlib.import_module('.' + i, package='plugins')
+        except Exception as e:
+            logger.warning("导入插件 {} 失败！ 原因:{}".format(i, e))
+    return plugins
+
+
 # 主函数
 def main():
     work_path = os.path.abspath(os.path.dirname(__file__))
     yaml_path = os.path.join(work_path, 'config.yml')
+    plugins_path = os.path.join(work_path, "plugins")
 
     logger.info(f"MuRain Bot开始运行，当前版本：{VERSION}")
     logger.info("Code with by Xiaosu & Evan. Copyright (c) 2024 GuppyTEAM. All rights reserved.")
@@ -60,6 +88,11 @@ def main():
 
     config = import_profile(yaml_path)
 
+    import_plugins(plugins_path)
+    logger.info("插件导入完成，共成功导入 {} 个插件".format(len(plugins)))
+    # for plugin in plugins:
+    #     plugin.PluginInfo
+
     logger.info("读取到监听ip，将以此ip启动监听服务器:{}:{}"
                 .format(config["api"]["host"], config["api"]["port"]))
 
@@ -68,7 +101,6 @@ def main():
     logger.info("启动监听服务器")
     server.serve_forever()
     logger.warning("监听服务器结束运行！")
-
 
 if __name__ == '__main__':
     pass
