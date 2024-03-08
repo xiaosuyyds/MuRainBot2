d7faa81827327294c7a96a9ee07cca4fc3df52c8
xiaosuyyds <Xiaosu-1009@qq.com>
2024-03-09 02:47:50 +0800

    feat(master): 新增关键词注册
    新增关键词注册，多种匹配模式，参考了rainybot的关键词注册。

--- Lib/EventManager.py
+++ Lib/EventManager.py
@@ -6,16 +6,13 @@
 #  |_|  |_|\__,_|_| \_\__,_|_|_| |_| |____/ \___/ \__|_____|
 
 import threading
+import re
+
+register_event_list = []  # event_type, func, arg, args
+register_keyword_list = []  # keyword, func, arg, args
 
 
-def cmp(x):
-    return x[2]
-
-
-register_list = []  # event_type, func, arg, args
-
-
-def register_event(event_type: [str, str], func, arg: int = 0, *args):
+def register_event(event_type: [str, str] | list[[str, str]], func, arg: int = 0, *args):
     """
     注册事件
     :param event_type: 接收的事件类型，可以是一个字符串，也可以是一个列表，列表中的字符串会依次匹配
@@ -24,10 +21,44 @@ def register_event(event_type: [str, str], func, arg: int = 0, *args):
     :param args: 传给func的参数
     :return: None
     """
+
+    def cmp(x):
+        return x[2]
+
     if args is None:
         args = []
-    register_list.append((event_type, func, arg, args))
-    register_list.sort(key=cmp, reverse=True)
+    if event_type is list:
+        for event in event_type:
+            if event is list:
+                register_event_list.append((event, func, arg, args))
+    register_event_list.append((event_type, func, arg, args))
+    register_event_list.sort(key=cmp, reverse=True)
+
+
+def register_keyword(keyword, func, model: str = "INCLUDE", arg: int = 0, *args):
+    """
+    注册关键字
+    :param keyword: 关键词
+    :param func: 触发后调用的函数
+    :param model: 匹配模式，支持BEGIN：仅当关键词位于消息开头时触发判定
+                             END：仅当关键词位于消息末尾时触发判定
+                             INCLUDE： 消息中只要包含关键词就触发判定
+                             EXCLUDE： 消息中不包含关键词就触发判定
+                             EQUAL： 消息与关键词完全匹配时触发判定
+                             REGEX： 消息满足正则表达式时触发判定（此时关键词内容应为一个正则表达式）
+                             默认INCLUDE
+    :param arg: 优先级，默认0
+    :param args: 传给func的参数
+    :return: None
+    """
+
+    def cmp(x):
+        return x[2]
+
+    if args is None:
+        args = []
+    register_keyword_list.append((keyword, func, model, arg, args))
+    register_keyword_list.sort(key=cmp, reverse=True)
 
 
 class Event:
@@ -41,9 +72,36 @@ class Event:
     def __init__(self, event_type: [str, str], event_data):
         self.event_class = event_type
         self.event_data = event_data
-        for event_class, func, arg, args in register_list:
+        # 事件扫描
+        for event_class, func, arg, args in register_event_list:
             if event_class == self.event_class or event_class == ["", ""] or event_class == "":
                 threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+
+        # 关键词检测
+        if self.event_class is list:
+            if self.event_class[0] == "message":
+                message = str(self.event_data["raw_message"])
+                for keyword, func, model, arg, args in register_keyword_list:
+                    if model == "BEGIN":
+                        if message.startswith(keyword):
+                            threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+                    elif model == "END":
+                        if message.endswith(keyword):
+                            threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+                    elif model == "INCLUDE":
+                        if keyword in message:
+                            threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+                    elif model == "EXCLUDE":
+                        if keyword not in message:
+                            threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+                    elif model == "EQUAL":
+                        if message == keyword:
+                            threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+                    elif model == "REGEX":
+                        if re.search(keyword, message):
+                            threading.Thread(target=lambda: func(self.event_class, self.event_data, *args)).start()
+                    else:
+                        raise ValueError("Unsupported model: {}".format(model))
 
 
 # 单元测试
