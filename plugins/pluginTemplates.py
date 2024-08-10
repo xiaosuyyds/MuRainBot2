af72f6b4be623a3140833623bbce232b6f67c669
xiaosuyyds <Xiaosu-1009@qq.com>
2024-08-10 18:34:59 +0800

    稍微优化一下示例插件

--- plugins/pluginTemplates.py
+++ plugins/pluginTemplates.py
@@ -6,7 +6,6 @@
 #  |_|  |_|\__,_|_| \_\__,_|_|_| |_| |____/ \___/ \__|_____|
 
 from Lib import *
-import logging
 
 api = OnebotAPI.OnebotAPI()
 
@@ -22,13 +21,12 @@ class PluginInfo:
 
 
 # 写法1: 注册关键词
-def say_hello(event_class, event_data):
-    if event_data.post_type == "message":
-        if event_data.message_type == "private":
-            BotController.send_message("Hello World!", user_id=event_data.user_id)
-        else:
-            BotController.send_message(QQRichText.QQRichText(QQRichText.At(event_data.user_id), "Hello World!"),
-                                       group_id=event_data.group_id)
+def say_hello(event_class, event_data: BotController.Event):
+    if event_data.message_type == "private":  # 判断是群聊事件还是私聊事件
+        BotController.send_message("Hello World!", user_id=event_data.user_id)
+    else:
+        BotController.send_message(QQRichText.QQRichText(QQRichText.At(event_data.user_id), "Hello World!"),
+                                   group_id=event_data.group_id)
 
 
 EventManager.register_keyword("hello", say_hello)
@@ -37,7 +35,7 @@ EventManager.register_keyword("hello", say_hello)
 # 写法2: 主函数
 def main(report, work_path):
     """
-    :param report: go-cqhttp上报的消息
+    :param report: 实现端上报的消息
     :param work_path: main.py所在的路径
     :return:
     """
@@ -54,8 +52,8 @@ def main(report, work_path):
 
 # 写法3: 注册事件
 @EventManager.register_event("message")
-def on_message(event_class, event_data):
-    if event_data.message == "hello":
+def on_message(event_class, event_data: BotController.Event):
+    if event_data.message == "hello":  # 判断是群聊事件还是私聊事件
         if event_data.message_type == "private":
             BotController.send_message("Hello World!", user_id=event_data.user_id)
         else:
