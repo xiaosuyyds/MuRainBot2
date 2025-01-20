30834128b261dbb182ec2be24fba949491904cc6
xiaosuyyds <Xiaosu-1009@qq.com>
2025-01-20 09:47:03 +0800

    refactor(Helper): 使用新的语法重构帮助插件

--- plugins/Helper.py
+++ plugins/Helper.py
@@ -5,67 +5,69 @@
 #  |_|  |_|\__,_|_| \_\__,_|_|_| |_| |____/ \___/ \__|
 # Code with by Xiaosu. Copyright (c) 2023 Guppy team. All rights reserved.
 # 本代码由由校溯写。版权所有 （c） 2023 Guppy团队。保留所有权利。
-import Lib.PluginManager
+
 from Lib import *
+from Lib.core import PluginManager
 
+logger = Logger.get_logger()
 
-class PluginInfo(PluginManager.PluginInfo):
-    def __init__(self):
-        super().__init__()
-        self.NAME = "Helper"  # 插件名称
-        self.AUTHOR = "校溯"  # 插件作者
-        self.VERSION = "1.0"  # 插件版本
-        self.DESCRIPTION = "帮助插件"  # 插件描述
-        self.HELP_MSG = "发送/help，或者/帮助即可查看帮助"  # 插件帮助
+plugin_info = PluginManager.PluginInfo(
+    NAME="Helper",
+    AUTHOR="Xiaosu",
+    VERSION="1.0.0",
+    DESCRIPTION="用于获取插件帮助信息",
+    HELP_MSG="发送/help或/帮助以获取全部的插件帮助信息"
+)
 
 
 def get_help_text():
-    plugins = Lib.PluginManager.plugins
+    plugins = PluginManager.plugins
     text = "沐云bot帮助"
     for plugin in plugins:
         try:
-            plugin_info = plugin["plugin"].PluginInfo()
+            plugin_info = plugin["info"]
             if plugin_info.DESCRIPTION and plugin_info.IS_HIDDEN is False:
                 text += f"\n{plugin_info.NAME} - {plugin_info.DESCRIPTION}"
-        # except Exception as e:
-        except:
-            # print(repr(e))
-            pass
+        except Exception as e:
+            logger.warning(f"获取插件{plugin['name']}信息时发生错误: {repr(e)}")
     text += "\n----------\n发送/help <插件名>或/帮助 <插件名>以获取插件详细帮助信息"
     return text
 
 
-def help(event_type, event_data):
+rule = EventHandlers.KeyValueRule("message", "/help", "func",
+                                  lambda a, b: a.startswith("/帮助") or a.startswith("/help"))
+
+matcher = EventHandlers.on_event(EventClassifier.GroupMessageEvent, priority=0, rules=[rule])
+
+
+@matcher.register_handler()
+def on_help(event_data):
     if event_data.message == "/帮助" or event_data.message == "/help":
-        BotController.send_message(
-            QQRichText.QQRichText(
+        Actions.SendMsg(
+            message=QQRichText.QQRichText(
                 QQRichText.Reply(event_data["message_id"]),
                 get_help_text()
             ), group_id=event_data["group_id"]
-        )
+        ).call()
     else:
         plugin_name = str(event_data.message).split(" ", 1)[1].lower()
-        for plugin in Lib.PluginManager.plugins:
+        for plugin in PluginManager.plugins:
             try:
-                plugin_info = plugin["plugin"].PluginInfo()
+                plugin_info = plugin["info"]
                 if plugin_info.NAME.lower() == plugin_name and plugin_info.IS_HIDDEN is False:
-                    BotController.send_message(
-                        QQRichText.QQRichText(
+                    Actions.SendMsg(
+                        message=QQRichText.QQRichText(
                             QQRichText.Reply(event_data["message_id"]),
                             plugin_info.HELP_MSG + "\n----------\n发送/help以获取全部的插件帮助信息"
                         ), group_id=event_data["group_id"]
-                    )
+                    ).call()
                     return
             except:
                 pass
         else:
-            BotController.send_message(
-                QQRichText.QQRichText(
+            Actions.SendMsg(
+                message=QQRichText.QQRichText(
                     QQRichText.Reply(event_data["message_id"]),
                     "没有找到此插件，请检查是否有拼写错误"
                 ), group_id=event_data["group_id"]
-            )
-
-
-EventManager.register_keyword("/help", help, model="BEGIN")
-EventManager.register_keyword("/帮助", help, model="BEGIN")
+            ).call()
