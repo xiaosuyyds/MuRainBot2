a50a5cf0dbde0b744dd469cdcebdd97ca6cfdf2c
xiaosuyyds <Xiaosu-1009@qq.com>
2025-01-23 15:27:05 +0800

    fix(core): 捕获异步任务中的异常并记录到日志
    - 新增 _wrapper函数用于捕获异步任务中的异常
    - 修改 async_task装饰器，使用 _wrapper包装原始函数
    - 异常信息包括错误类型、错误值和堆栈跟踪，便于排查问题

--- Lib/core/ThreadPool.py
+++ Lib/core/ThreadPool.py
@@ -1,5 +1,6 @@
 # Created by BigCookie233
-
+import sys
+import traceback
 from concurrent.futures import ThreadPoolExecutor
 from Lib.core.ConfigManager import GlobalConfig
 from Lib.utils.Logger import get_logger
@@ -24,10 +25,23 @@ def init():
     atexit.register(shutdown)
 
 
+def _wrapper(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+
+        # 打印到日志中
+        logger.error(
+            f"Error in async task({func.__module__}.{func.__name__}): {repr(e)}\n"
+            f"{"".join(traceback.format_exception(exc_type, exc_value, exc_traceback))}"
+        )
+
+
 def async_task(func):
     def wrapper(*args, **kwargs):
         if isinstance(thread_pool, ThreadPoolExecutor):
-            return thread_pool.submit(func, *args, **kwargs)
+            return thread_pool.submit(_wrapper, func, *args, **kwargs)
         else:
             logger.warning("Thread Pool is not initialized. Please call init() before using it.")
             return func(*args, **kwargs)
